455a37f90c1fb2ad8a736673a447d287ec66e42f
AndrewIvlev <mr.andrew.ivlev@gmail.com>
2019-07-04 13:06:59 +0300

    Init FSharp project

---
+++ C++/tests/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+
+TEST(TestCaseName, TestName) {
+	// -3 2 -1 1 1
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
--- tests/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-
-TEST(TestCaseName, TestName) {
-	// -3 2 -1 1 1
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
